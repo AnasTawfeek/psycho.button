6b0a54d217ac1352cefb1d9d2d21823b53ff3bf0
Anas Tawfeek <anas.tawfeek@gmail.com>
2017-09-16 20:37:59 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import styles from './styles.scss';
+
+class PsychoButton extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <button className="PsychoButton">
+                Hola
+            </button>
+        )
+    }
+}
+
+export default PsychoButton;
